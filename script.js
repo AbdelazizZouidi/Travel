965cd7e85c8ba518e32822e7124036b0cf57d511
AbdelazizZouidi <abdelazizezd@gmail.com>
2020-07-19 21:47:01 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+Array.from(document.querySelectorAll
+(".navigation-button")).forEach(item => {
+    item.onclick = () =>{
+     item.parentElement.parentElement.classList.
+     toggle("change");
+    };
+});
